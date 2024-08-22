e488b58923c554a5de2ceae6ebec46f8cd7bcd4f
Antuhrium <antyriyman@gmail.com>
2024-08-22 23:40:47 +0300

    add: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.scss";
 import Header from "./sections/Header/Header";
+import HeroSection from "./sections/HeroSection/HeroSection";
 
 function App() {
     return (
         <main className={styles.container}>
             <Header />
+            <HeroSection />
         </main>
     );
 }
