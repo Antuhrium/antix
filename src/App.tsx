87ad91734db5874d4d7ec972aa78d1c815d6af3d
ayanhan <kalkhozhaev.j@gmail.com>
2024-09-08 04:21:29 +0900

    fix: added components 1-3

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Footer from "./sections/Footer/Footer";
 import Header from "./sections/Header/Header";
 import HeroSection from "./sections/HeroSection/HeroSection";
 import TokenomicsSection from "./sections/TokenomicsSection/TokenomicsSection";
+import RoadMap from "./sections/RoadMap/Roadmap";
+import UserFlow from "./sections/UserFlow/UserFlow";
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
             <Header />
             <HeroSection />
             <TokenomicsSection />
+            <RoadMap />
+            <UserFlow />
             <Footer />
         </main>
     );
