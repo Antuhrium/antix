4679294ee7905301f1dde9e77fe128f93189fcf9
ayanhan <64448894+ayanhan@users.noreply.github.com>
2024-09-12 07:19:22 -0700

    fix(section): antix token (#17)

--- src/sections/TokenInfo/TokenInfo.tsx
+++ src/sections/TokenInfo/TokenInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styles from './TokenInfo.module.scss';
 
 interface PersonProps {
@@ -7,94 +7,106 @@ interface PersonProps {
     title: string;
     boldDescription: string;
     description: string;
+    boldFirst?: boolean;
 }
 
-const TokenInfoCard: React.FC<PersonProps> = ({ backgroundImage, image, title, boldDescription, description }) => (
+const TokenInfoCard: React.FC<PersonProps> = ({ backgroundImage, image, title, boldDescription, description, boldFirst = true }) => (
     <div
         className={styles.tokenInfoCard}
         style={{
             backgroundImage: `url(${backgroundImage})`,
         }}
     >
-        <div className={styles.imageContainer}>
-            <img src={image} alt={title} />
-        </div>
         <h3 className={styles.title}>{title}</h3>
-        <p className={styles.boldDescription}>{boldDescription}</p>
-        <p className={styles.description}>{description}</p>
-    </div >
+        <div className={styles.descriptionContainer}>
+            {boldFirst ? (
+                <>
+                    <span className={styles.boldDescription}>{boldDescription}</span>
+                    {" "}
+                    <span className={styles.description}>{description}</span>
+                </>
+            ) : (
+                <>
+                    <span className={styles.description}>{description}</span>
+                    {" "}
+                    <span className={styles.boldDescription}>{boldDescription}</span>
+                </>
+            )}
+        </div>
+        <img src={image} alt={title} className={styles.cornerImage} />
+    </div>
 );
 
 const TokenInfo = () => {
-    const scrollRef = useRef<HTMLDivElement>(null);
-
-    const TokenInfos = [
+    const TokenInfosLeft = [
         {
             backgroundImage: '/tokeninfo/bg.png',
-            image: '/tokeninfo/1.png',
+            image: '/tokeninfo/sub.png',
             title: 'Subscriptions',
             boldDescription: 'Unlock premium tools',
             description: 'for digital creation with ANTIX.',
+            boldFirst: true,
         },
         {
             backgroundImage: '/tokeninfo/bg.png',
-            image: '/tokeninfo/2.png',
-            title: 'Marketplace',
-            boldDescription: 'Trade and customize',
-            description: 'your digital assets in the marketplace.',
-        },
-        {
-            backgroundImage: '/tokeninfo/bg.png',
-            image: '/tokeninfo/3.png',
-            title: 'Staking',
-            boldDescription: 'Stake your tokens and boost your rewards.',
-            description: 'Higher stake, higher earnings.',
-        },
-        {
-            backgroundImage: '/tokeninfo/bg.png',
-            image: '/tokeninfo/4.png',
-            title: 'Asset Tuning',
-            boldDescription: 'Upgrade your digital assets —',
-            description: 'keep them trendy and fresh.',
-        },
-        {
-            backgroundImage: '/tokeninfo/bg.png',
-            image: '/tokeninfo/5.png',
+            image: '/tokeninfo/nft.png',
             title: 'NFT Auction & Promos',
             boldDescription: 'Lend or sell avatars and assets.',
             description: 'Promote to earn more.',
+            boldFirst: false,
         },
         {
             backgroundImage: '/tokeninfo/bg.png',
-            image: '/tokeninfo/6.png',
+            image: '/tokeninfo/governance.png',
             title: 'Governance',
             boldDescription: 'Vote on creators and rewards.',
             description: 'Shape the future with ANTIX.',
+            boldFirst: false,
         },
     ];
 
-    useEffect(() => {
-        const scrollContainer = scrollRef.current;
-        if (!scrollContainer) return;
-
-        const handleScroll = () => {
-            const { scrollLeft, scrollWidth, clientWidth } = scrollContainer;
-
-            if (scrollLeft === 0) {
-                scrollContainer.scrollLeft = scrollWidth / 3;
-            } else if (scrollLeft + clientWidth >= scrollWidth - 1) {
-                scrollContainer.scrollLeft = scrollWidth / 3;
-            }
-        };
-
-        scrollContainer.addEventListener('scroll', handleScroll);
-        return () => scrollContainer.removeEventListener('scroll', handleScroll);
-    }, []);
+    const TokenInfosRight = [
+        {
+            backgroundImage: '/tokeninfo/bg.png',
+            image: '/tokeninfo/asset.png',
+            title: 'Asset Tuning',
+            boldDescription: 'Upgrade your digital assets —',
+            description: 'keep them trendy and fresh.',
+            boldFirst: true,
+        },
+        {
+            backgroundImage: '/tokeninfo/bg.png',
+            image: '/tokeninfo/staking.png',
+            title: 'Staking',
+            boldDescription: 'Stake your tokens and boost your rewards.',
+            description: 'Higher stake, higher earnings.',
+            boldFirst: false,
+        },
+        {
+            backgroundImage: '/tokeninfo/bg.png',
+            image: '/tokeninfo/marketplace.png',
+            title: 'Marketplace',
+            boldDescription: 'Trade and customize',
+            description: 'your digital assets in the marketplace.',
+            boldFirst: true,
+        },
+    ]
 
     return (
         <div className={styles.container}>
-            <div className={styles.containerHeader}>
-                <h1>Token Info</h1>
+            <div className={styles.containerLeft}>
+                {TokenInfosLeft.map((tokenInfo, index) => (
+                    <TokenInfoCard key={index} {...tokenInfo} />
+                ))}
+            </div>
+
+            <div className={styles.containerCenter}>
+                <div className={styles.mark}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="314" viewBox="0 0 355 364" fill="none">
+                        <path d="M32.937 0.942122L34.1888 0C60.3035 31.4076 134.701 116.715 177.486 116.715C210.535 116.715 229.566 66.7615 232.156 59.5442L233.664 60.0693C233.664 60.0693 209.927 126.314 232.236 143.768C254.174 160.929 346.494 131.956 354.396 129.46L354.846 130.961C354.846 130.961 248.757 163.159 242.016 193.846C237.196 215.78 264.826 246.751 272.202 254.697L271.091 255.764C271.091 255.764 238.985 219.843 215.089 229.884C179.556 244.823 159.24 349.29 156.78 364L155.213 363.706C155.213 363.706 177.505 245.985 121.67 219.582C90.3038 204.741 12.8856 223.679 0.603536 226.744L0.15332 225.207C0.15332 225.207 108.437 198.121 108.437 138.577C108.437 90.3823 32.937 0.942122 32.937 0.942122Z" fill="white" />
+                    </svg>
+                </div>
+                <h1>ANTIX Token</h1>
                 <div className={styles.buttons}>
                     <button>Whitepaper</button>
                     <button>Audit report</button>
@@ -110,9 +122,9 @@ const TokenInfo = () => {
                 </div>
             </div>
 
-            <div className={styles.infiniteHorizontalScroll} ref={scrollRef}>
-                {[...TokenInfos, ...TokenInfos, ...TokenInfos].map((info, index) => (
-                    <TokenInfoCard key={index} {...info} />
+            <div className={styles.containerRight}>
+                {TokenInfosRight.map((tokenInfo, index) => (
+                    <TokenInfoCard key={index} {...tokenInfo} />
                 ))}
             </div>
         </div>
