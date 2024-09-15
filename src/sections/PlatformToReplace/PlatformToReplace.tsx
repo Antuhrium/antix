36fc3dbe44fde02a78fe4a58ea817f70e3dc1b8d
Antuhrium <antyriyman@gmail.com>
2024-09-15 22:33:24 +0300

    added animation to slider

--- src/sections/PlatformToReplace/PlatformToReplace.tsx
+++ src/sections/PlatformToReplace/PlatformToReplace.tsx
@@ -1,23 +1,19 @@
-import { useEffect, useState, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
 import styles from "./PlatformToReplace.module.scss";
 
-const words = ["influencers", "actors", "TV hosts"];
+const words = [
+    "influencers",
+    "actors",
+    "TV hosts",
+    "influencers",
+    "actors",
+    "TV hosts",
+];
 
 const PlatformToReplace = () => {
-    const [displayWords, setDisplayWords] = useState(words);
-    const containerRef = useRef<HTMLDivElement>(null);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDisplayWords((prevWords) => {
-                const [first, ...rest] = prevWords;
-                return [...rest, first];
-            });
-        }, 4000);
-
-        return () => clearInterval(intervalId);
-    }, []);
-
     return (
         <section className={styles.wrapper}>
             <div className={styles.container}>
@@ -25,16 +21,28 @@ const PlatformToReplace = () => {
                     <span>Reshaping a $300 billion industry.</span>
                     <h4>Platform for digital humans to replace:</h4>
                 </div>
-                <div className={styles.carousel} ref={containerRef}>
-                    {displayWords.map((word, index) => (
-                        <span
-                            key={word}
-                            className={index === 1 ? styles.active : ""}
-                        >
-                            {word}
-                        </span>
+                <Swiper
+                    direction={"vertical"}
+                    slidesPerView={3}
+                    loop={true}
+                    centeredSlides={true}
+                    autoplay={{ delay: 4000 }}
+                    modules={[Autoplay]}
+                    allowTouchMove={false}
+                    noSwiping={true}
+                    keyboard={{ enabled: false }}
+                    className={styles.carousel}
+                >
+                    {words.map((word) => (
+                        <SwiperSlide key={word} className={styles.slide}>
+                            {({ isActive }) => (
+                                <span className={isActive ? styles.active : ""}>
+                                    {word}
+                                </span>
+                            )}
+                        </SwiperSlide>
                     ))}
-                </div>
+                </Swiper>
             </div>
         </section>
     );
