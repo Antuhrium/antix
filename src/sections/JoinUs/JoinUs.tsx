a057229db38d6f72fc4f0e00023e74ad9b920a03
ayanhan <64448894+ayanhan@users.noreply.github.com>
2024-09-16 05:11:34 -0700

    Ayan/fix acronix (#46)
    * fix: acronix name
    
    * fix: email name

--- src/sections/JoinUs/JoinUs.tsx
+++ src/sections/JoinUs/JoinUs.tsx
@@ -13,13 +13,13 @@ const JoinUs: React.FC = () => {
             </div>
             <div className={styles.cardsContainer}>
                 <div className={styles.cardTopLeft}>
-                    <FollowerCard platform="x" handle="@acronix" followers={44000} />
+                    <FollowerCard platform="x" handle="@antix" followers={44000} />
                 </div>
                 <div className={styles.cardBottomCenter}>
-                    <FollowerCard platform="discord" handle="@acronix" followers={12000} />
+                    <FollowerCard platform="discord" handle="@antix" followers={12000} />
                 </div>
                 <div className={styles.cardTopRight}>
-                    <FollowerCard platform="telegram" handle="@acronix" followers={75000} />
+                    <FollowerCard platform="telegram" handle="@antix" followers={75000} />
                 </div>
             </div>
         </div>
