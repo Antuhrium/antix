df8fa305d21855d197fdeb611981c88c2d114ab8
ayanhan <kalkhozhaev.j@gmail.com>
2024-09-20 19:23:50 +0900

    fix: small fixes

--- src/sections/JoinUs/JoinUs.tsx
+++ src/sections/JoinUs/JoinUs.tsx
@@ -16,13 +16,13 @@ const JoinUs: React.FC = () => {
             </div>
             <div className={styles.cardsContainer}>
                 <div className={styles.cardTopLeft}>
-                    <FollowerCard platform="x" link='https://x.com/antix_in' handle="@antix" followers={44000} />
+                    <FollowerCard platform="x" link='https://x.com/antix_in' handle="@antix" followers={40900} />
                 </div>
                 <div className={styles.cardBottomCenter}>
-                    <FollowerCard platform="discord" link="https://discord.com/invite/bKcMXChRRT" handle="@antix" followers={12000} />
+                    <FollowerCard platform="discord" link="https://discord.com/invite/bKcMXChRRT" handle="@antix" followers={2015} />
                 </div>
                 <div className={styles.cardTopRight}>
-                    <FollowerCard platform="telegram" link="https://t.me/antix_in" handle="@antix" followers={75000} />
+                    <FollowerCard platform="telegram" link="https://t.me/antix_in" handle="@antix" followers={65819} />
                 </div>
             </div>
         </div>
